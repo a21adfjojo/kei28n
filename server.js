e4f4f7ae59d8bdf7fb688eef2fc7a0906dc38cd2
a21adfjojo <shinyashiki244drive@gmail.com>
2025-07-09 16:37:32 +0900

    a

--- server.js
+++ server.js
@@ -124,7 +124,8 @@ app.get("/api/threads/:id/posts", async (req, res) => {
     if (!thread)
       return res.status(404).json({ error: "スレッドが見つかりません。" });
 
-    const posts = await Post.find({ threadId }).sort({ createdAt: 1 });
+    const posts = await Post.find({ threadId }).sort({ createdAt: -1 }); // 新しい順
+
     res.json({ thread, posts });
   } catch (err) {
     res.status(500).json({ error: err.message });
